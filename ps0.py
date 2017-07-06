ad405ed0cbbbeba9623aa6754ddc9fdf8c43ce16
Taylor Harvey <taylorharvey@Taylors-MacBook.local>
2017-07-06 21:59:46 +0530

    wrapped code in a function

--- ps0.py
+++ ps0.py
@@ -1,5 +1,11 @@
-print ("Enter your date of birth.")
-date_of_birth = input()
-print ("Enter your last name.")
-last_name = input()
-print (last_name + ' ' + date_of_birth)
+def main():
+    """
+    This asks questions then prints answers according to user replies.
+    """
+    print ("Enter your date of birth.")
+    date_of_birth = input()
+    print ("Enter your last name.")
+    last_name = input()
+    print (last_name + ' ' + date_of_birth)
+
+main()
