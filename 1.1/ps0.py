863e19ad9aed6f4c1507f5ade11bb984f77b81e1
Taylor Harvey <taylorharvey@Taylors-MacBook.local>
2017-07-07 20:46:13 +0530

    Updated Commit

---
+++ 1.1/ps0.py
@@ -0,0 +1,11 @@
+def main():
+    """
+    This asks questions then prints answers according to user replies.
+    """
+    print ("Enter your date of birth.")
+    date_of_birth = input()
+    print ("Enter your last name.")
+    last_name = input()
+    print (last_name + ' ' + date_of_birth)
+
+main()
