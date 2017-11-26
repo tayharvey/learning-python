7674922d6f9a61ad4fbeee5914f30e44e6e5a995
Taylor Harvey <taylorharvey@Taylors-MacBook.local>
2017-11-26 15:06:44 +0100

    Comment placement fixed in ex3.py

--- ex3.py
+++ ex3.py
@@ -1,37 +1,38 @@
-print("I will now count my chickens.")
 #This line prints the statement.
+print("I will now count my chickens.")
 
-print("Hens", 25 + 30 / 6)
 #This line prints the number of hens calculated by the operation.
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25 + 30 / 6)
+
 #This line prints the number of roosters calculated by the operation.
+print("Roosters", 100 - 25 * 3 % 4)
 
-print("Now I will count the eggs:")
 #This prints the statement.
+print("Now I will count the eggs:")
 
-print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 #This prints the calculation, giving the number of eggs.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
-print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
 #This prints the question.
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
 
-print(3.0 + 2.0 < 5.0 - 7.0)
 #This prints if the statement is true or false.
+print(3.0 + 2.0 < 5.0 - 7.0)
 
+#This prints the question and answer.
 print("What is 3.0 + 2.0?", 3.0 + 2.0)
 #This prints the question and answer.
 print("What is 5.0 - 7.0?", 5.0 - 7.0)
-#This prints the question and answer.
 
+#This prints a statement.
 print("Oh, that's why it's False.")
-#This prints a statement.
 
+#This prints a statement.
 print("How about some more.")
-#This prints a statement.
 
+#This prints true or false.
 print("Is it greater?", 5.0 > -2.0)
 #This prints true or false.
 print("Is it greater or equal?", 5.0 >= -2.0)
 #This prints true or false.
 print("Is it less or equal?", 5.0 <= -2.0)
-#This prints true or false.
