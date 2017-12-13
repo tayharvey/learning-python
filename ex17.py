a89bbdfdd8cbc45c15cfb2de8c3a0a4f3a2045c5
Taylor Harvey <taylorharvey@Taylors-MacBook.local>
2017-12-13 16:24:49 +0100

    reverted to original ex17, not satisfied with script

--- ex17.py
+++ ex17.py
@@ -4,4 +4,19 @@ from os.path import exists
 script, from_file, to_file = argv
 
 # we could do these two on one line - how?
-open(from_file, 'w').write(open(to_file).read())
+in_file = open(from_file)
+indata = in_file.read()
+
+print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file really exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
