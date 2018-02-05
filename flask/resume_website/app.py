2f4f1c3b99bf0d2049c7f65aa094b52c0bfefc67
Taylor Harvey <taylorharvey@Taylors-MacBook.local>
2018-02-05 16:12:32 +0100

    Updated app to only include URL path to resume website

--- flask/resume_website/app.py
+++ flask/resume_website/app.py
@@ -1,21 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Index!"
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
+@app.route("/resume/taylorharvey")
+def taylorharvey():
+    return "Taylor Olivia Harvey"
 
-@app.route("/members")
-def members():
-    return "Members"
-
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
 
 if __name__ == "__main__":
     app.run()
